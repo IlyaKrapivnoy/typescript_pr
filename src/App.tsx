c4986df4f80bb7e6d0ad7d2692dbeb7d4deb3abf
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2022-01-04 18:56:21 +0200

    BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { NavBar } from './components/NavBar';
+import { AboutPage } from './pages/AboutPage';
+import { TodosPage } from './pages/TodosPage';
 
 const App: React.FC = () => {
     return (
-        <>
+        <BrowserRouter>
             <NavBar />
-            <div className='container'></div>
-        </>
+            <div className='container'>
+                <Switch>
+                    <Route component={TodosPage} path='/' exact />
+                    <Route component={AboutPage} path='/about' />
+                </Switch>
+            </div>
+        </BrowserRouter>
     );
 };
 
