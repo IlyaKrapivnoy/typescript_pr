aa7bff628a61aa4bdd21e0b052a136bed248b7d4
Kath <ilya.krapivnoy@gmail.com>
2022-01-04 17:05:04 +0200

    add todoFormProps

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar } from './components/NavBar';
 import { TodoForm } from './TodoForm';
 
 const App: React.FC = () => {
+    const [todos, setTodos] = useState([]);
+
+    const addHandler = (title: string) => {
+        console.log('Add new todo');
+    };
+
     return (
         <>
             <Navbar />
             <div className='container'>
-                <TodoForm />
+                <TodoForm onAdd={addHandler} />
             </div>
         </>
     );
