566808398555462dbaff323afdea2c71c6bdd35a
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2022-01-04 18:38:48 +0200

    AboutPage

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,3 +1,15 @@
 import React from 'react';
 
-export const AboutPage: React.FC = () => {};
+export const AboutPage: React.FC = () => {
+    return (
+        <>
+            <h1>Страница Информации</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
+                officia blanditiis nam totam ipsum ex odit sequi dolorem
+                corrupti.
+            </p>
+            <button className='btn'>К списку дел</button>
+        </>
+    );
+};
